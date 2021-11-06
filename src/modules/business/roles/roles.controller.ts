fac249f591ddd539ec1577354945ee1a21a4b531
jpb06 <jpb.06@outlook.com>
2021-11-06 21:29:41 +0100

    Refactoring existing routes

---
+++ src/modules/business/roles/roles.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get } from '@nestjs/common';
+import { transformTo } from '@util/transform-to';
+
+import { RolesStoreService } from '@db/stores/roles-store.service';
+import { ApiRoute } from '@decorators/api-route';
+
+import { RolesResultDto } from './roles/roles.result.dto';
+
+@Controller('roles')
+export class RolesController {
+  constructor(private readonly rolesStore: RolesStoreService) {}
+
+  @Get()
+  @ApiRoute({
+    summary: 'Gets all roles',
+    ok: {
+      type: RolesResultDto,
+      description: 'The available roles',
+    },
+  })
+  async getAllRoles(): Promise<RolesResultDto> {
+    const data = await this.rolesStore.getAll();
+
+    return transformTo(RolesResultDto, data);
+  }
+}
